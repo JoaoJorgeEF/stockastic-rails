baf7a26f14c46c7079fc5a01c646d68e6e26f79c
JoaoJorgeEF <joaojorgeevangelista@gmail.com>
2023-12-07 16:52:35 -0300

    add tipo to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name username type])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name username type])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name username tipo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name username tipo])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_165059) do
     t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "type"
+    t.string "tipo"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
